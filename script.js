617f9049eaf9a92ca592fa306f0ccb6532a1b5e6
sapavath-lokesh7 <88229694+sapavath-lokesh7@users.noreply.github.com>
2023-10-29 19:35:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Include the qrcode-generator library
-// You can download it from: https://github.com/davidshimjs/qrcodejs
-// Or include it from a CDN
-
-// Generate the QR code
 document.getElementById("generate-btn").addEventListener("click", function () {
   const text = document.getElementById("text-input").value;
   if (text.trim() !== "") {
